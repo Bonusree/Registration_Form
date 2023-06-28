ebc9295b087604af954f15cd014bfb4bf7fc5bd6
Bonusree <bonusreedatta820@gmail.com>
2023-06-28 13:06:50 +0600

    change wsgi.py

--- Registration_Form/wsgi.py
+++ Registration_Form/wsgi.py
@@ -1,21 +1,15 @@
 """
-WSGI config for Registration_Form project.
+WSGI config for books_and_question project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-from django.core.wsgi import get_wsgi_application
-project_home = '/home/Bonusree/Registration_Form'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
 
-# set environment variable to tell django where your settings.py is
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'Registration_Form.settings'
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Registration_Form.settings')
 
